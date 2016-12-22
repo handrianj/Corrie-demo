5fb543e1307ee3567cac65181ef2ee5209d7a329
Heri Andrianjafy <andrianjafy.heri@mpsa.com>
2016-12-22 09:52:49 +0800

    Creating the initial demo

---
+++ org.handrianj.corrie.demo/src/org/handrianj/demo/DemoEditorInput.java
@@ -0,0 +1,19 @@
+package org.handrianj.demo;
+
+import org.handrianj.corrie.editors.util.editors.AbstractCorrieEditorInput;
+
+public class DemoEditorInput extends AbstractCorrieEditorInput<Object> {
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getToolTipText() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ org.handrianj.corrie.demo/src/org/handrianj/demo/DemoView.java
@@ -0,0 +1,27 @@
+package org.handrianj.demo;
+
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+import org.handrianj.corrie.viewsmanager.ui.ICorrieView;
+
+public class DemoView extends ViewPart implements ICorrieView {
+
+	@Override
+	public void updateInput(Object input) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void createPartControl(Composite parent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setFocus() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
